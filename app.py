47372aa1ea1556935c87dbb927a0bbb1c05ebbac
Lifenotgoodxd <lifenotgoodxd@gmail.com>
2024-08-08 11:49:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from threading import Thread
+from flask import Flask
+import os
+import time
+
+PORT = int(os.environ.get('PORT', 7070))  # Use the PORT environment variable
+start_time = time.time()
+bot = Flask(__name__)
+
+@bot.route('/')
+def home():
+    end_time = time.time()
+    uptime_seconds = end_time - start_time
+    uptime_minutes = uptime_seconds / 60
+    return f"Bot uptime: {uptime_minutes:.2f} minutes"
+
+def run():
+    bot.run(host='0.0.0.0', port=PORT)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
